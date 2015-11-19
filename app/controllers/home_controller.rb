20e45b245908a6f4b764b5655ca6708f65d05c77
jaeyoonkim <kimjy888@gmail.com>
2015-11-19 15:22:17 +0000

    add_process controller & html

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ApplicationController
+  def list
+  end
+
+  def add
+  end
+
+  def add_process
+		n = Number.new
+		n.name = params[:phone_user]
+		n.phone_number = params[:phone_number]
+  	n.save
+	end
+end
